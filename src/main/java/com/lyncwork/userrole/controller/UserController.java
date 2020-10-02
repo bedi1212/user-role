75ddfea34aa821ce3a8c25647f6152338c9f1519
bedi <bedi.9494@gmail.com>
2020-10-02 11:00:06 -0400

    commit changes

--- src/main/java/com/lyncwork/userrole/controller/UserController.java
+++ src/main/java/com/lyncwork/userrole/controller/UserController.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+
 
 @RestController
 @RequestMapping("/user")
